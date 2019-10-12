cb3402c3f7fb2d223192bed2eda624375b62dd87
bsdpunk <bsdpunk@gmail.com>
2019-10-12 12:08:37 -0500

    TablesStruct

---
+++ shell/tables/tables.go
@@ -0,0 +1,79 @@
+package Monsters
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/elgs/jsonql"
+	h "github.com/mitchellh/go-homedir"
+	"github.com/tatsushid/go-prettytable"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+var Home, _ = h.Dir()
+
+type Table struct {
+}
+
+type Tables struct {
+	Tables     []Table
+	jsonString string
+}
+
+func (ms *Tables) List() {
+
+	for i := range ms.Tables {
+		fmt.Print((ms.Tables[i].Index - 1))
+		fmt.Println(" " + ms.Tables[i].Name)
+	}
+	return
+
+}
+
+func (ms *Tables) TableById(id string) {
+	n, _ := strconv.Atoi(id)
+	fmt.Println(ms.Tables[n]) //.Strength)
+	return
+}
+
+func (ms *Tables) SBById(id string) {
+	n, _ := strconv.Atoi(id)
+	ms.Tables[n].StatBlock() //.Strength)
+	return
+}
+
+func (ms *Tables) ByName(name string) {
+	parser, err := jsonql.NewStringQuery(ms.jsonString)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(parser.Query("name='" + name + "'"))
+}
+
+func (ms *Tables) ByGuess(name string) {
+	parser, err := jsonql.NewStringQuery(ms.jsonString)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(parser.Query("name~='" + name + "'"))
+}
+
+func (ms *Tables) Load() {
+	jsonFile, err := os.Open(Home + "/go/src/github.com/bsdpunk/dndshell/json/monsters.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	ms.jsonString = string(byteValue)
+	if err := json.Unmarshal(byteValue, &ms.Tables); err != nil {
+		fmt.Println(err)
+	}
+	//json.Unmarshal(byteValue, &ms)
+	return
+}
